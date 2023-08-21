70d35032478cf0b682b39f47c4c7ec404ec2e71f
chlalswns200 <chlalswns200@naver.com>
2023-08-21 17:52:12 +0900

    3-1 영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,9 @@
 package hellojpa;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -9,9 +11,34 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager entityManager = emf.createEntityManager();
-        entityManager.close();
-        emf.close();
 
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA2");
+
+            //영속
+            System.out.println("=== BEFORE ===");
+            entityManager.persist(member);
+            System.out.println("=== AFTER ===");
+
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+
+        entityManager.close();
+
+        emf.close();
 
 
     }
